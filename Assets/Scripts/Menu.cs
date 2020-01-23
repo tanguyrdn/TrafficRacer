92be191ce1400ccfeebff3a5d160e9e2085145cd
bastien458 <44195182+bastien458@users.noreply.github.com>
2020-01-23 09:49:15 +0100

    'zer'

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   public void playGame()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+}
